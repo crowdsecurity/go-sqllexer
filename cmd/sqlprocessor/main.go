d54a327ae7e681cf06d87296375575efcb19a316
mazzma12 <matthieu@crowdsec.net>
2026-03-11 11:17:11 +0100

    Revert "Feat: remove SPACE from model"
    This reverts commit 03e6e43e370e0504165cfcef70817217c83f60e0.

--- cmd/sqlprocessor/main.go
+++ cmd/sqlprocessor/main.go
@@ -288,10 +288,6 @@ func tokenizeLineTypesOnly(line string) string {
 		if tok.Type == sqllexer.EOF {
 			break
 		}
-		// SPACE don't bring information
-		if tok.Type == sqllexer.SPACE {
-			continue
-		}
 		types = append(types, tokenTypeName(tok.Type))
 	}
 	return strings.Join(types, " ")
@@ -309,10 +305,6 @@ func tokenizeLine(line string, lineNum int) record {
 		}
 		if tok.Type == sqllexer.ERROR {
 			hasError = true
-		}
-		// SPACE don't bring information
-		if tok.Type == sqllexer.SPACE {
-			continue
 		}
 		tokens = append(tokens, tokenOut{
 			Type:  tokenTypeName(tok.Type),
